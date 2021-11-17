c11fd05b6b17aaf08a45f465317fa3b5c7f22c84
Sergiy <ipisaryev@gmail.com>
2021-11-17 13:54:15 +0200

    initial commit

---
+++ wdio.conf.js
@@ -0,0 +1,28 @@
+exports.config = {
+    specs: [
+        './specs/**/*.spec.js'
+    ],
+    maxInstances: 1,
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+    }],
+
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+
+    connectionRetryTimeout: 30000,
+    connectionRetryCount: 1,
+
+    services: ['devtools'],
+    framework: 'mocha',
+    reporters: ['spec'],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
