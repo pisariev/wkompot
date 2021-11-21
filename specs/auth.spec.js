ceeaa53ffcb2f848234f390320b57ae63f860935
pisariev <86107013+pisariev@users.noreply.github.com>
2021-11-21 11:13:08 +0200

    add beforeEach hooks for auth spec
    Co-authored-by: Sergiy <ipisaryev@gmail.com>

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -2,8 +2,11 @@ import LoginPage from '../pages/login.page';
 import ProfilePage from '../pages/profile.page';
 
 describe('Auth', function() {
-    it('Successful log in', async function() {
+    beforeEach(async function() {
         await LoginPage.open();
+    });
+
+    it('Successful log in', async function() {
         await expect(LoginPage.buttonSubmit)
             .toBeDisabled();
         await LoginPage.login('ipisaryev@gmail.com', '31101967Serg');
