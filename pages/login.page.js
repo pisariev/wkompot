f1fae2c0e087a112c69cb028b7f2e862abc9f918
Sergiy <ipisaryev@gmail.com>
2021-11-24 12:47:33 +0200

    add setup.helper.js add more tests in auth.spec.js add more in login.page.js add Command in wdio.conf.js

--- pages/login.page.js
+++ pages/login.page.js
@@ -4,6 +4,12 @@ class LoginPage extends Page {
     get inputUsername() { return $('[qa-id="email"]') }
     get inputPassword() { return $('[qa-id="password"]') }
     get buttonSubmit() { return $('.login-form-button') }
+    get notification() { return $('.ant-notification-notice-closable')}
+    get emailMessage() { return $('//div[contains(text(),"\'email\' is not a valid email")]')}
+    get passwordValidation() { return $('//div[contains(text(),\'Required\')]')}
+    get emailValidation() { return $('//form[1]/div[1]/div[1]/div[2]/div[1]')}
+    get passwordMessage() { return $('.ant-notification-notice-closable')}
+
 
     async login(username, password) {
         await this.inputUsername.setValue(username);
